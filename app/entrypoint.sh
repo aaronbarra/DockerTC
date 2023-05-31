d8287c13ec5182fb6b7b741fd61aa167afbfa006
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-05-31 12:44:18 -0400

    Create entrypoint.sh

---
+++ app/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+REPOSITORIO=$1
+TAG=$2
+cd /opt
+ls
+chmod +x clone.sh
+sh clone.sh ${REPOSITORIO}
+cd /opt/framework
+chmod +x testgradle.sh
+sh testgradle.sh ${TAG}
+chmod +x xray.sh
+sh xray.sh
