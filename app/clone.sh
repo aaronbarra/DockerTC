3c40458259538e0423f24db63666510a7a929528
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-06-02 17:27:28 -0400

    Update clone.sh

--- app/clone.sh
+++ app/clone.sh
@@ -7,7 +7,7 @@ cd /opt/framework/
 export token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
 if [ "$?" -ne 0 ]; then
     echo "Error al clonar el repositorio "
-    sed -i 's/"summary": "TITULOJSON"/"summary": "Clonacion fallida"/g' integracion/jira.json
+    sed -i 's/"summary": "TITULOJSON"/"summary": "Clonacion fallida"/g' Info.json
     curl -H "Content-Type: multipart/form-data" -X POST -F info=@Info.json -F results=@integracion/prueba-failed.json -H "Authorization: Bearer $token" https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart
     exit 1
 fi
