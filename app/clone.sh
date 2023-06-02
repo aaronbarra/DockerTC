551c2cdb93339696e696610b6bb42fc5681a4d87
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-06-02 17:05:18 -0400

    Update clone.sh

--- app/clone.sh
+++ app/clone.sh
@@ -3,10 +3,14 @@ RAMA=$1
 rm -R /opt/framework
 mkdir /opt/framework
 git clone -b $RAMA https://github.com/aaronbarra/Framework_Automatizacion.git /opt/framework
-
+cd /opt/framework/
+export token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" https://xray.cloud.getxray.app/api/v2/authenticate| tr -d '"')
 if [ "$?" -ne 0 ]; then
     echo "Error al clonar el repositorio "
+    sed -i 's/"summary": "TITULOJSON"/"summary": "Clonacion fallida"/g' integracion/jira.json
+    curl -H "Content-Type: multipart/form-data" -X POST -F info=@Info.json -F results=@integracion/prueba-failed.json -H "Authorization: Bearer $token" https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart
     exit 1
 fi
-
 echo "Repositorio clonado con éxito"
+sed -i 's/"summary": "TITULOJSON"/"summary": "Clonacion exitosa"/g' integracion/jira.json
+curl -H "Content-Type: multipart/form-data" -X POST -F info=@Info.json -F results=@integracion/prueba-passed.json -H "Authorization: Bearer $token" https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart
