313e213fdf18305769bfbc487ad718f2d69de7be
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-06-02 12:17:26 -0400

    Update clone.sh

--- app/clone.sh
+++ app/clone.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-REPO_URL=$1
+RAMA=$1
 rm -R /opt/framework
 mkdir /opt/framework
-git clone $REPO_URL /opt/framework
+git clone -b $RAMA https://github.com/aaronbarra/Framework_Automatizacion.git /opt/framework
 
 if [ "$?" -ne 0 ]; then
-    echo "Error al clonar el repositorio $REPO_URL"
+    echo "Error al clonar el repositorio "
     exit 1
 fi
 
-echo "Repositorio $REPO_URL clonado con éxito"
+echo "Repositorio clonado con éxito"
