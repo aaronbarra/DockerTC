4d983e2e116477f125bf4677ad2652d09a1ea6bd
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-06-05 10:18:34 -0400

    Update clone.sh

--- app/clone.sh
+++ app/clone.sh
@@ -14,4 +14,3 @@ fi
 echo "Repositorio clonado con éxito"
 sed -i 's/"summary": "TITULOJSON"/"summary": "Clonacion exitosa"/g' integracion/jira.json
 curl -H "Content-Type: multipart/form-data" -X POST -F info=@integracion/jira.json -F results=@integracion/prueba-passed.json -H "Authorization: Bearer $token" https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart
-ls 
